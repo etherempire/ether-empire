eb4db5bb3640b8b19a62d5128a0e7786eea664f8
Jacky Liu <Jacky35liu@gmail.com>
2021-05-08 16:23:08 -0500

    added ability to switch to MATIC TESTNET

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
+/*
 import React, { Component } from "react";
-import {getWeb3, isMetaMaskInstalled, isMetaMaskConnected, isAddressConnected, getExistingWeb3} from "./getWeb3";
+import {getWeb3, isMetaMaskInstalled, isMetaMaskConnected, isAddressConnected, getExistingWeb3, maticWeb3} from "./getWeb3";
 import Main from "./webapp/src/components/Pages/Main";
 import EtherEmpireContract from "./contracts/EtherEmpireWorld.json"
 
@@ -22,10 +23,12 @@ class App extends Component {
       const accounts = await web3.eth.getAccounts();
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = EtherEmpireContract.networks[networkId];
+      
       const instance = new web3.eth.Contract(
         EtherEmpireContract.abi,
          deployedNetwork && deployedNetwork.address,
       );
+      
       this.setState({ web3, accounts, instance});
     } catch (error) {
       console.error(error);
@@ -48,6 +51,67 @@ class App extends Component {
           installed={isMetaMaskInstalled} 
           web3={this.state} 
           connectWeb3={this.connectWeb3}/>
+      </div>
+    )
+  }
+}
+*/
+
+
+import React, { Component } from "react";
+import { getWeb3, isMetaMaskInstalled, isMetaMaskConnected, isAddressConnected, getExistingWeb3, getMaticWeb3, switchToMatic } from "./getWeb3";
+import Main from "./webapp/src/components/Pages/Main";
+import EtherEmpireContract from "./contracts/EtherEmpireWorld.json"
+
+import "./App.css";
+
+class App extends Component {
+  state = { web3: null, accounts: null, instance: null };
+
+  connectWeb3 = async () => {
+    try {
+      const web3 = await getWeb3();
+      await this.setWeb3(web3)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  setWeb3 = async (web3) => {
+    try {
+      // await switchToMatic();                          // <-- UNCOMMENT TO SWITCH TO MATIC TESTNET
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = EtherEmpireContract.networks[networkId];
+
+      const instance = new web3.eth.Contract(
+        EtherEmpireContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      this.setState({ web3, accounts, instance });
+      console.log("web3", web3, "accounts", accounts, "instance", instance);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  homeConnected() {
+    return this.state.accounts && this.state.accounts.length != 0
+  }
+
+  componentDidMount = async () => {
+    await this.setWeb3(getExistingWeb3())
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Main
+          connected={this.state.accounts && this.state.accounts.length != 0}
+          installed={isMetaMaskInstalled}
+          web3={this.state}
+          connectWeb3={this.connectWeb3} />
       </div>
     )
   }
